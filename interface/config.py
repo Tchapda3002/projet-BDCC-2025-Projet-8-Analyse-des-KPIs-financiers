f2b8e3ad88f94e7c5465dfc000f01497195aef4d
Wilfred TCHAPDA <Apple@192.168.1.3>
2025-12-06 00:03:16 +0000

    streamlit folders

---
+++ interface/config.py
@@ -0,0 +1,14 @@
+import yaml
+import os
+
+config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'config.yaml')
+with open(config_path, 'r') as f:
+    CONFIG = yaml.safe_load(f)
+
+ENV = {
+    'project_id': CONFIG['gcp']['project_id'],
+    'region': CONFIG['gcp']['region'],
+    'bucket': CONFIG['storage']['bucket_name'],
+    'dataset': CONFIG['bigquery']['dataset'],
+    'credentials': CONFIG['gcp'].get('credentials_path', '')
+}
